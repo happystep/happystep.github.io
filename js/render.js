9a564d008477b4598e1243f952729631b2544dc4
Luis <happystep@ksu.edu>
2017-05-01 08:06:39 -0500

    changing movement speed again x2

--- js/render.js
+++ js/render.js
@@ -4,7 +4,7 @@
 
 //variables we need for it
 
-var movementSpeed = 50; //we should have an on click event to dynamically change this variable
+var movementSpeed = 200; //we should have an on click event to dynamically change this variable
 var totalObjects = 30;
 var objectSize = 20;
 var sizeRandomness = 10; //since we are setting x and y initial for the particles to 0, we don't need thi variable but lets keep it for now
@@ -112,12 +112,12 @@ function render() {
 }
 
 function leftArrowPressed(){
-  movementSpeed -= 0.5;
+  movementSpeed -= 10;
     render();
 }
 
 function rightArrowPressed(){
-  movementSpeed += 0.5;
+  movementSpeed += 10;
     render();
 }
 
